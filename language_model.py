10b2b382db6995457914c243781391a765eb6330
alex kelly <kelster101757@hotmail.com>
2023-11-22 16:39:03 +0000

    cant get repsonces to work

--- language_model.py
+++ language_model.py
@@ -3,8 +3,10 @@ import requests
 import json
 from openai import OpenAI
 
-# Your OpenAI API key
-api_key = ''
+# Read the API key from the file
+with open('api_key.txt', 'r') as file:
+    api_key = file.read().strip()
+
 # This code is for v1 of the openai package: pypi.org/project/openai
 client = OpenAI(api_key=api_key)
 
@@ -25,17 +27,26 @@ def read_text_file(file_path):
         content = file.read()
     return content
 
-def summerise(message):
-    response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system", "content": prompt_text},
-            {"role": "user", "content": message}
-        ],
-        temperature=0,
-        max_tokens=100000 
-    )
-    return response
+def summarize(message):
+    # Split the message into smaller chunks of 4096 tokens each
+    chunks = [message[i:i+4096] for i in range(0, len(message), 4096)]
+    # Initialize an empty list to store the summaries
+    summaries = []
+    # Loop through each chunk and send a request to the Completions API
+    for chunk in chunks:
+        response = client.completions.create(
+            model="gpt-4-1106-preview",  # Replace with an appropriate model name
+            prompt=chunk,
+            temperature=0,
+            max_tokens=8000
+        )
+        # Extract the summary from the response and append it to the list
+        summary = response.choices[0].text
+        summaries.append(summary)
+    # Join the summaries into a single string and return it
+    return " ".join(summaries)
+
+
 
 prompt_text = read_text_file('summary_prompt.txt')
 
@@ -45,6 +56,6 @@ file_data = read_files_in_folder(folder_path)
 # Prepare data for API call (example: sending one file's contents at a time)
 for content in file_data:
     data = content
-    response = summerise(data)
+    response = summarize(data)
     details = response.choices[0].message.content
     print(details)  # handle the details as needed
